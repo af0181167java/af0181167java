35ce5e6d9509e35c137d2fa4feb60d817b77989e
Kamal1167/java <103422837+af0181167java@users.noreply.github.com>
2022-04-27 12:05:32 +0530

    Create AlphabetPattern3.java

---
+++ DAILY ASSIGENMENT/AlphabetPattern3.java
@@ -0,0 +1,17 @@
+class AlphabetPattern3
+{
+public static void main (String [] args)
+{
+int i,j,row = 5 ;
+int alphabet = 65; 
+for ( i = 0; i<= row; i++)
+{
+for(j=0; j<=i; j++)
+{
+System.out.print((char) (alphabet+i)+"   ");
+}
+System.out.println();
+}
+}
+}
+© 2022 GitHub
