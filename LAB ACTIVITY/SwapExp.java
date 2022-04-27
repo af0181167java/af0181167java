94cf83f1f1362c10f63611ae5375e61f81fddd0f
Kamal1167/java <103422837+af0181167java@users.noreply.github.com>
2022-04-27 12:22:34 +0530

    Create SwapExp.java

---
+++ LAB ACTIVITY/SwapExp.java
@@ -0,0 +1,13 @@
+public class SwapExp{
+static public  void main (String [] args)
+{
+int a = 10, b =12;
+System.out.println("numbers before swapping" +a + " " +b);
+a = a+b;
+b = a-b;
+a = a-b;
+System.out.println("numbers after swapping " +a + " " +b);
+
+}
+}
+© 2022 GitH
